2134086a0f252406e83a370f704e28287148e6f9
axan <akanbaimuratov96@icloud.com>
2023-04-17 14:10:35 +0600

    C# 005

---
+++ C#_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "аружан")
+{                     
+    Console.WriteLine("Ура, это же АРУЖАН!");
+}
+
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
